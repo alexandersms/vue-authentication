67c2a75fdbc4af11b3cde50d442814eb9f16d5bc
Alex Simisi Nta <smsxander@gmail.com>
2022-01-01 06:11:02 +0100

    create members and contact pages

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,17 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/members",
+    name: "Members",
+    component: () => import("../views/Members.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
   },
 ];
 
